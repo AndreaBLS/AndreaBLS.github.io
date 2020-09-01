d00bb28c2e8aa52f4102d462595469539eef3aaf
Matmuret <filippo.moretti6@gmail.com>
2020-09-01 15:40:44 +0200

    images

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
-import Card from "./components/Card";
 import Artist from "./components/Artist";
+import Card from "./components/Card"
 
 import "./App.scss";
 
-function App() {
+export default class App extends React.Component {
+  state={ 
+    images:[
+    { id:"1.jpg"},
+    { id:"2.jpg"},
+    { id:"3.jpg"},
+    { id:"4.jpg"},
+    { id:"5.jpg"}
+  ]
+  }
+ 
+  render(){
   return (
     <Router>
       <div className="App">
@@ -20,9 +31,16 @@ function App() {
           <Route path="/artist" component={Artist} />
         </Switch>
         </header>
+        <div className="cards">
+        {this.state.images.map((item)=>{
+          return(<Card key ={item} image={item.id}/>)
+    
+  })}
+  </div>
       </div>
     </Router>
   );
 }
+}
 
-export default App;
+
