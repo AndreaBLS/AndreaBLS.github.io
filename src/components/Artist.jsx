fbf6fc7df5f7e0fa27e2eef99c28da7b85a24695
AndreaBLS <frostbittenblash@gmail.com>
2020-09-02 15:26:11 +0200

    filled Artist.jsx with data from parent

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -7,9 +7,30 @@ export default function Artist(props) {
       <div className="ArtistImage">
         <img src={props.location.artistData.image} alt="artist" />
       </div>
-      <h1>{props.location.artistData.name}</h1>
+      <h2>{props.location.artistData.name}</h2>
       <p>{props.location.artistData.bio}</p>
-      
-    </div>
+      <div className="albumContainer">
+      <div>
+        <h3>Album Name: {props.location.artistData.albums.album1.albumName}</h3>
+        <h3>Price: {props.location.artistData.albums.album1.albumPrice}€</h3>
+        <h3>Release Year: {props.location.artistData.albums.album1.albumYear}</h3>
+        <img src={props.location.artistData.albums.album1.albumImage}></img>        
+        </div>
+
+        <div>
+        <h3>Album Name: {props.location.artistData.albums.album2.albumName}</h3>
+        <h3>Price: {props.location.artistData.albums.album2.albumPrice}€</h3>
+        <h3>Release Year: {props.location.artistData.albums.album2.albumYear}</h3>
+        <img src={props.location.artistData.albums.album2.albumImage}></img>        
+        </div>
+
+        <div>
+        <h3>Album Name: {props.location.artistData.albums.album3.albumName}</h3>
+        <h3>Price: {props.location.artistData.albums.album3.albumPrice}€</h3>
+        <h3>Release Year: {props.location.artistData.albums.album3.albumYear}</h3>
+        <img src={props.location.artistData.albums.album3.albumImage}></img>        
+        </div>
+      </div>
+   </div>
   );
 }
