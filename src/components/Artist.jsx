476114ca7dfc5f6f9d579c99389b709997ee883d
Matmuret <filippo.moretti6@gmail.com>
2020-09-02 16:15:46 +0200

    .

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -3,34 +3,59 @@ import React from "react";
 export default function Artist(props) {
   return (
     <div>
-      <h1>Artist</h1>
-      <div className="ArtistImage">
-        <img src={props.location.artistData.image} alt="artist" />
-      </div>
       <h2>{props.location.artistData.name}</h2>
-      <p>{props.location.artistData.bio}</p>
-      <div className="albumContainer">
-      <div>
-        <h3>Album Name: {props.location.artistData.albums.album1.albumName}</h3>
-        <h3>Price: {props.location.artistData.albums.album1.albumPrice}€</h3>
-        <h3>Release Year: {props.location.artistData.albums.album1.albumYear}</h3>
-        <img src={props.location.artistData.albums.album1.albumImage}></img>        
+
+      <div className="artistBigContainer">
+
+        <div className="artistImage2">
+          <img src={props.location.artistData.image} alt="artist" />
         </div>
 
-        <div>
-        <h3>Album Name: {props.location.artistData.albums.album2.albumName}</h3>
-        <h3>Price: {props.location.artistData.albums.album2.albumPrice}€</h3>
-        <h3>Release Year: {props.location.artistData.albums.album2.albumYear}</h3>
-        <img src={props.location.artistData.albums.album2.albumImage}></img>        
+        <div className="bioContainer">
+          <p>{props.location.artistData.bio}</p>
         </div>
 
-        <div>
-        <h3>Album Name: {props.location.artistData.albums.album3.albumName}</h3>
-        <h3>Price: {props.location.artistData.albums.album3.albumPrice}€</h3>
-        <h3>Release Year: {props.location.artistData.albums.album3.albumYear}</h3>
-        <img src={props.location.artistData.albums.album3.albumImage}></img>        
+        <div className="albumContainer">
+          <div>
+            <h3>
+              Album Name: {props.location.artistData.albums.album1.albumName}
+            </h3>
+            <h3>
+              Price: {props.location.artistData.albums.album1.albumPrice}€
+            </h3>
+            <h3>
+              Release Year: {props.location.artistData.albums.album1.albumYear}
+            </h3>
+            <img src={props.location.artistData.albums.album1.albumImage}></img>
+          </div>
+
+          <div>
+            <h3>
+              Album Name: {props.location.artistData.albums.album2.albumName}
+            </h3>
+            <h3>
+              Price: {props.location.artistData.albums.album2.albumPrice}€
+            </h3>
+            <h3>
+              Release Year: {props.location.artistData.albums.album2.albumYear}
+            </h3>
+            <img src={props.location.artistData.albums.album2.albumImage}></img>
+          </div>
+
+          <div>
+            <h3>
+              Album Name: {props.location.artistData.albums.album3.albumName}
+            </h3>
+            <h3>
+              Price: {props.location.artistData.albums.album3.albumPrice}€
+            </h3>
+            <h3>
+              Release Year: {props.location.artistData.albums.album3.albumYear}
+            </h3>
+            <img src={props.location.artistData.albums.album3.albumImage}></img>
+          </div>
         </div>
       </div>
-   </div>
+    </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,16 +7,17 @@ export default function Card ({ image, name, bio, albums }) {
 console.log(image)
   return (
     <div className="card">
-      <div className="image">
+      
+      <h2>{name}</h2>
         <Link to={{ pathname: "/artist", artistData:{image,bio,name,albums }, }}>
-          <div className="ArtistImage">
+          <div className="artistImage">
             <img src={image}  alt="imag44e" />
           </div>
         </Link>
-      </div>
+     
 
       <div className="content">
-        <h1>{name}</h1>
+        
         <p></p>
       </div>
     </div>
