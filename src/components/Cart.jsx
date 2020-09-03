21d004327c6730f9eeb23610561113dc5db06dbf
AndreaBLS <frostbittenblash@gmail.com>
2020-09-03 12:14:34 +0200

    workin functionalities, style needed

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,27 +5,29 @@ export default function Cart({ addedcartAlbums }) {
   console.log();
   return (
     <div>
-      <div className="display">
+      <div className="displayCart">
         <h1>{addedcartAlbums && addedcartAlbums.length}</h1>
-          {addedcartAlbums.map((item) => {
+        <div className="cartHeaderContainer">
+          <h2>Album Artist</h2>
+          <h2>Album Name</h2>
+          <h2>Album Year</h2>
+          <h2>Album Price</h2>
+          <h2>album image</h2>
+        </div>
 
-            return (
-              <div className="albumContainer">
-              <h2>Album Artist</h2>
-              
+        {addedcartAlbums.map((item) => {
+          return (
+            <div className="albumContainerArtist">
               <h2>{item.albumArtist} </h2>
               <h2>{item.albumName}</h2>
               <h2>{item.albumYear}</h2>
               <h2>{item.albumPrice} €</h2>
               <div className="cartAlbumContainer">
-              <img src={item.albumImage} alt=""/>
+                <img src={item.albumImage} alt="" />
               </div>
-              </div>
-            )
-            
-
-            
-          })}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
