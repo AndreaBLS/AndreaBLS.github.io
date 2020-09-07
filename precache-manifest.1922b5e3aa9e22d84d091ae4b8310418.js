539305404ba9e8903d1cc30c6f80b82085e278f7
AndreaBLS <frostbittenblash@gmail.com>
2020-09-07 08:51:41 +0200

    Updates

---
+++ precache-manifest.1922b5e3aa9e22d84d091ae4b8310418.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d533300a3a02dff4b58ed1778bc8637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54b5e12409eda2283654",
+    "url": "/static/css/main.f63ecc38.chunk.css"
+  },
+  {
+    "revision": "ad38e87183e9a5b31763",
+    "url": "/static/js/2.78d9ade6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.78d9ade6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b5e12409eda2283654",
+    "url": "/static/js/main.508ea717.chunk.js"
+  },
+  {
+    "revision": "f934f06ec39d336a2307",
+    "url": "/static/js/runtime-main.7c9ee0cc.js"
+  },
+  {
+    "revision": "3759ae846dd1c47989379a69573f933e",
+    "url": "/static/media/logo.3759ae84.jpg"
+  }
+]);
